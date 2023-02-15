33f678ca4d3239acae64938558c64ee57cbdcb09
Zenonist <phonarnun@hotmail.com>
2023-02-15 18:35:45 +0000

    feat: show letter when press letter

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,21 @@ import { boardDefault } from "./Words";
 
 export const AppContext = createContext();
 
+type currAttemptType = {
+    attempt: number,
+    letterPos: number,
+}
 
 function App() {
     // update the board array
     const [board, setBoard] = useState(boardDefault)
+    const [currAttempt, setCurrAttempt] = useState<currAttemptType>({attempt: 0, letterPos: 0})
     return (
         <div className="App">
             <nav>
                 <h1>Wordle</h1>
             </nav>
-            <AppContext.Provider value={{board, setBoard}}>
+            <AppContext.Provider value={{board, setBoard, currAttempt, setCurrAttempt}}>
                 {/* style: center all components*/}
                 <div className="game">
                     <Board />
--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../App'
 
 type KeyParam = {
     keyVal: String,
@@ -6,8 +7,32 @@ type KeyParam = {
 }
 
 export default function Key(_KeyParam: KeyParam) {
+    const { board, setBoard, currAttempt, setCurrAttempt } = useContext(AppContext);
+    const selectLetter = () => {
+        if (_KeyParam.keyVal === "ENTER") {
+            // * if letterPos is not 5, then return the end function (Let user insert the rest of the letters)
+            if (currAttempt.letterPos !== 5) return;
+            setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0});
+        } else if (_KeyParam.keyVal === "DELETE") {
+            if (currAttempt.letterPos === 0) return;
+            const newBoard = [...board];
+            newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+            setBoard(newBoard);
+            setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+        } else {
+            console.log(currAttempt.letterPos);
+            // if letterPos is greater than 4, then return the end function
+            if (currAttempt.letterPos > 4) return;
+            const newBoard = [...board];
+            newBoard[currAttempt.attempt][currAttempt.letterPos] = _KeyParam.keyVal;
+            setBoard(newBoard);
+            setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+        }
+
+    }
+
     return (
         // if bigKey is true, then id="big"
-        <div className="key" id={_KeyParam.bigKey && "big"}>{_KeyParam.keyVal}</div>
+        <div className="key" id={_KeyParam.bigKey && "big"} onClick={selectLetter} >{_KeyParam.keyVal}</div>
     )
 }
