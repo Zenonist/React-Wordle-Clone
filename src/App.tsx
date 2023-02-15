5cce608eeef8ce688660d0234c7dd0031261b075
Zenonist <phonarnun@hotmail.com>
2023-02-15 11:35:16 +0000

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <nav>
+                <h1>Wordle</h1>
+            </nav>
+            <p>test</p>
+        </div>
+    );
+}
+
+export default App;
